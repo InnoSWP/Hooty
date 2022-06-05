f9e811dccc4cbf0e1ddfbf03305e89bad368e5c0
Mierley <mierer.lei@gmail.com>
2022-06-05 18:35:11 +0300

    Changed models

---
+++ Innohoot/Models/ElementsForPA/Statistic.cs
@@ -0,0 +1,16 @@
+﻿using Innohoot.Models.Activity;
+
+namespace Innohoot.Models.ElementsForPA
+{
+	public class Statistic
+	{
+		public Guid UserId { get; set; }
+		public string Name { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime StarTime { get; set; }
+		public string Description { get; set; }
+		public List<Poll> Polls { get; set; }
+		public bool Available { get; set; } = false;
+		public Guid Id { get; set; }
+	}
+}
