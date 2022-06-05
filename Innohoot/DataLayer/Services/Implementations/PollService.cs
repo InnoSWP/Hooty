02981aa43d35a7b8d352241ab06d04a64151c37d
Mierley <mierer.lei@gmail.com>
2022-06-05 19:55:48 +0300

    Added PollService

---
+++ Innohoot/DataLayer/Services/Implementations/PollService.cs
@@ -0,0 +1,42 @@
+﻿using Innohoot.Models.Activity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Innohoot.DataLayer.Services.Implementations
+{
+	public class PollService
+	{
+		private readonly IDBRepository _db;
+		public PollService(IDBRepository repository)
+		{
+			_db = repository;
+		}
+		public async Task<Guid> Create(Poll poll )
+		{
+			await _db.Add(poll);
+			await _db.Save();
+			return poll.Id;
+		}
+
+		public async Task Delete(Guid pollId)
+		{
+			await _db.Delete<Poll>(pollId);
+			await _db.Save();
+		}
+
+		public async Task<Poll?> Get(Guid pollId)
+		{
+			return await _db.Get<Poll>(pollId).FirstOrDefaultAsync();
+		}
+
+		public async Task<List<Poll>?> GetAllPollsByUserId(Guid userId)
+		{
+			return await _db.GetAll<Poll>().Where(poll => poll.UserId.Equals(userId)).ToListAsync();
+		}
+		public async Task Update(Poll poll)
+		{
+			await _db.Update(poll);
+			await _db.Save();
+		}
+    }
+}
