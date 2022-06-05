d32f0941570cf1f0bd00f28234b3b0a7190c6abe
Mierley <mierer.lei@gmail.com>
2022-06-05 19:55:48 +0300

    Added PollService

---
+++ Innohoot/DataLayer/Services/Implementations/IPollService.cs
@@ -0,0 +1,12 @@
+﻿using Innohoot.Models.Activity;
+
+namespace Innohoot.DataLayer.Services.Implementations;
+
+public interface IPollService
+{
+	Task<Guid> Create(Poll poll );
+	Task Delete(Guid pollId);
+	Task<Poll?> Get(Guid pollId);
+	Task<List<Poll>?> GetAllPollsByUserId(Guid userId);
+	Task Update(Poll poll);
+}
