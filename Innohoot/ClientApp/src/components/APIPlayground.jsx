2ebb58c818b672b2a8949e69d2f973a3c69e7f12
Tufra <60471036+Tufra@users.noreply.github.com>
2022-06-07 22:45:00 +0300

    api test page

--- Innohoot/ClientApp/src/components/APIPlayground.jsx
+++ Innohoot/ClientApp/src/components/APIPlayground.jsx
@@ -17,19 +17,25 @@ export default function APIPlayground(props) {
             headers: {
                 "Content-Type": parsedDetails.contentType
             },
-            body: parsedDetails.body
+            body: JSON.stringify(parsedDetails.body)
         } : {})
-            .then((result) => result.json())
-            .then((data) => setResult(data));
+            .then(
+                (result) => {return result.json()},
+                (err) =>  {return err}
+            )
+            .then(
+                (data) => {setResult(data)}, 
+                (err) => {setResult(err)}
+            );
     }
     
     return (
-        <div>
+        <div style={{border: "1px solid black", padding: "10px", width: "500px"}}>
             <h3>{props.name}</h3>
             <div>
                 <button onClick={() => requestData(props.url, props.details)}>Request</button>
             </div>
-            <textarea style={{width: "100%"}} name="" id="" cols="30" rows="10" value={result}></textarea>
+            <textarea style={{width: "100%"}} name="" id="" cols="30" rows="10" value={result} readOnly={true}></textarea>
         </div>
     )
 }
