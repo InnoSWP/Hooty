45de1a304ec03b4035de522bcc434febc3ebc432
Mierley <mierer.lei@gmail.com>
2022-06-07 16:09:38 +0300

    Added Controllers without implementation

---
+++ Innohoot/Controllers/PollController.cs
@@ -0,0 +1,24 @@
+﻿using Innohoot.DataLayer;
+using Innohoot.DataLayer.Services.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Innohoot.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PollController:Controller
+	{
+		private readonly IPollService _pollService;
+		private readonly ISessionService _sessionService;
+		private readonly IDBRepository _db;
+
+		public PollController(IPollService pollService, ISessionService sessionService, IDBRepository db)
+		{
+			_pollService = pollService;
+			_sessionService = sessionService;
+			_db = db;
+		}
+
+		
+	}
+}
