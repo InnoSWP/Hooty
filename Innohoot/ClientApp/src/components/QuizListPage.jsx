98ce47db624a01dfa2a7081b88815cbd85a55fa4
Tufra <60471036+Tufra@users.noreply.github.com>
2022-06-11 22:20:19 +0300

    #31 setquizlist

--- Innohoot/ClientApp/src/components/QuizListPage.jsx
+++ Innohoot/ClientApp/src/components/QuizListPage.jsx
@@ -44,6 +44,8 @@ export default function QuizListPage(props) {
                         })
                     }
                 })
+                
+                setQuizList(mappedQuizList)
             })
     }, [])
     
