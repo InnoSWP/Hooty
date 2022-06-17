28f89e196fd96c5920a4ea6b199941737106639c
Ruslan Khairov <habur331@gmail.com>
2022-06-17 17:25:10 +0300

    Added method to User Service for needs authorization
    It method gives user id by userDTO

--- Innohoot/DataLayer/Services/Interfaces/IUserService.cs
+++ Innohoot/DataLayer/Services/Interfaces/IUserService.cs
@@ -7,5 +7,11 @@ namespace Innohoot.DataLayer.Services.Interfaces
 		Task<Guid> Create(UserDTO userDTO);
 		Task Delete(Guid Id);
 		Task<UserDTO?> Get(Guid Id);
+		/// <summary>
+		/// Searches for a user by username and password hash.
+		/// </summary>
+		/// <param name="userDTO"></param>
+		/// <returns>Returns the user ID, if found. If not found, it returns null.</returns>
+		Task<Guid?> GetId(UserDTO userDTO);
 	}
 }
