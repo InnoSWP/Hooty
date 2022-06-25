966102598658385c37aa9aab96ff44f104981594
Mierley <88342799+Mierley@users.noreply.github.com>
2022-06-25 17:37:16 +0300

    #59 Database updating

---
+++ Innohoot/Migrations/20220625143217_ChangedPoll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Innohoot.Migrations
+{
+    public partial class ChangedPoll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "AnswerId",
+                table: "Polls",
+                type: "uuid[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerId",
+                table: "Polls");
+        }
+    }
+}
