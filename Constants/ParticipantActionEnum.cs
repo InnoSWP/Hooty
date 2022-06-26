d08dcb45aa8f6fba438c9c9491d4d3d7ac0a30a3
Mierley <88342799+Mierley@users.noreply.github.com>
2022-06-26 19:32:34 +0300

    #59 Added enum

---
+++ Constants/ParticipantActionEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+	public enum ParticipantActionEnum
+	{
+		SubmitVote = 1,
+		DisplayResults = 2
+
+	}
+}
