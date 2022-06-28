dfd80a8663791119d9f10feca70b9b30b6d2158d
Ruslan Khairov <habur331@gmail.com>
2022-06-28 12:16:26 +0300

    #66 Added migration
    It adds list of participants to Session entity

--- Innohoot/Migrations/20220628091451_ParticipantListInSession.Designer.cs
+++ Innohoot/Migrations/20220628091451_ParticipantListInSession.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Innohoot.DataLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Innohoot.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220628091451_ParticipantListInSession")]
+    partial class ParticipantListInSession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Innohoot/Migrations/20220628091451_ParticipantListInSession.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Innohoot.Migrations
+{
+    public partial class ParticipantListInSession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "ParticipantList",
+                table: "Sessions",
+                type: "text[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParticipantList",
+                table: "Sessions");
+        }
+    }
+}
