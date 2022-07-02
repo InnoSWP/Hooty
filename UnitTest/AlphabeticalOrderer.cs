e9a48c4d3eee68bf9289dfd6c94a3e29080054a6
Mierley <88342799+Mierley@users.noreply.github.com>
2022-07-02 14:05:43 +0300

    #878 Added Unit tests

---
+++ UnitTest/AlphabeticalOrderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace UnitTest
+{
+	public class AlphabeticalOrderer : ITestCaseOrderer
+	{
+		public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases)
+			where TTestCase : ITestCase
+		{
+			var result = testCases.ToList();
+			result.Sort((x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.TestMethod.Method.Name, y.TestMethod.Method.Name));
+			return result;
+		}
+	}
+}
